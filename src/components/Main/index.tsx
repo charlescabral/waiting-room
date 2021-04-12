7893d2548dc828d581c3580e9d4e472a8984bd3d
charlescabral <charles.cabral.neto@gmail.com>
2021-04-12 03:48:34 +0000

    Initial commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="frontlab.com.br.svg" alt="Frontlab . Coding Services" />
+    <S.Title>Frontlab</S.Title>
+    <S.Subtitle>Coding Services</S.Subtitle>
+  </S.Wrapper>
+)
+
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #050505;
+  color: #f7f7f7;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 4rem;
+`
+
+export const Title = styled.h1`
+  font-size: 4rem;
+  margin-bottom: 1rem;
+`
+
+export const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: #e6af20;
+`
+
+export const Logo = styled.img`
+  width: min(25rem, 100%);
+  margin-bottom: 6rem;
+`
